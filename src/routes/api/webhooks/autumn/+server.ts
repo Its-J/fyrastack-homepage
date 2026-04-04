f3b57dff9ac9fbbf4320053ba916d2cc49c644b9
Hero <caleb@calebgj.io>
2026-04-03 22:56:12 -0500

    start autumn webhook api route

---
+++ src/routes/api/webhooks/autumn/+server.ts
@@ -0,0 +1,20 @@
+import { error, type RequestHandler } from '@sveltejs/kit';
+import { Webhook } from "svix";
+
+export const POST: RequestHandler = ({ request }) => {
+
+	//
+
+	https: const min = Number(url.searchParams.get('min') ?? '0');
+	const max = Number(url.searchParams.get('max') ?? '1');
+
+	const d = max - min;
+
+	if (isNaN(d) || d < 0) {
+		error(400, 'min and max must be numbers, and min must be less than max');
+	}
+
+	const random = min + Math.random() * d;
+
+	return new Response(String(random));
+};
