5e077e15e1ebc1d8d319f513e0fd99b809ec38a1
Katherine <kat@fyralabs.com>
2026-04-03 23:37:51 -0500

    Update colocationPlans.ts

--- src/lib/data/colocationPlans.ts
+++ src/lib/data/colocationPlans.ts
@@ -3,8 +3,8 @@ export const colocationPlans = [
 	{ name: 'STACK-2U', price: '100', units: '2', power: '300', bandwidth: '1Gbps', ips: '1' },
 	{ name: 'STACK-3U', price: '140', units: '3', power: '400', bandwidth: '1Gbps', ips: '1' },
 	{ name: 'STACK-4U', price: '180', units: '4', power: '500', bandwidth: '1Gbps', ips: '1' },
-	{ name: 'STACK-5U', price: '240', units: '5', power: '600', bandwidth: '1Gbps', ips: '1' },
-	{ name: 'STACK-6U', price: '280', units: '6', power: '700', bandwidth: '1Gbps', ips: '1' }
+	{ name: 'STACK-5U', price: '220', units: '5', power: '600', bandwidth: '1Gbps', ips: '1' },
+	{ name: 'STACK-6U', price: '260', units: '6', power: '700', bandwidth: '1Gbps', ips: '1' }
 ] as const;
 
 export type ColocationPlan = (typeof colocationPlans)[number];
